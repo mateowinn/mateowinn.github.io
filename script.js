386738e04ceca87540475f2dd507ff12b7b793b1
mateowinn <mateowinn@gmail.com>
2017-11-07 22:40:10 -0800

    Adding Vue and a real direction experiment!

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
 var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
 
-var colors = [ 'aqua' , 'azure' , 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral', 'crimson', 'cyan', 'fuchsia', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'linen', 'magenta', 'maroon', 'moccasin', 'navy', 'olive', 'orange', 'orchid', 'peru', 'pink', 'plum', 'purple', 'red', 'salmon', 'sienna', 'silver', 'snow', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'white', 'yellow'];
+var colors = [ 'up', 'down', 'left', 'right', 'aqua' , 'azure' , 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral', 'crimson', 'cyan', 'fuchsia', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'linen', 'magenta', 'maroon', 'moccasin', 'navy', 'olive', 'orange', 'orchid', 'peru', 'pink', 'plum', 'purple', 'red', 'salmon', 'sienna', 'silver', 'snow', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'white', 'yellow'];
 var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
 
 var recognition = new SpeechRecognition();
@@ -25,6 +25,12 @@ colors.forEach(function(v, i, a){
 });
 hints.innerHTML = 'Tap/click then say a color to change the background color of the app. Try '+ colorHTML + '.';
 
+var state = {
+  color: 'blue',
+  direction: 'nowhere',
+  bottom: 0.5,
+  left: 0.5
+};
 document.body.onclick = function() {
   recognition.start();
   console.log('Ready to receive a color command.');
@@ -46,10 +52,20 @@ recognition.onresult = function(event) {
 
   console.log('Event results:', event.results);
   var last = event.results.length - 1;
-  var color = event.results[last][0].transcript;
+  var transcript = event.results[last][0].transcript;
+  switch (transcript) {
+    case 'up':
+    case 'down':
+    case 'left':
+    case 'right':
+      state.direction = transcript;
+      break;
+    default:
+      state.color = transcript;
+  }
 
-  diagnostic.textContent = 'Result received: ' + color + '.';
-  bg.style.backgroundColor = color;
+  diagnostic.textContent = 'Result received: ' + state.color + '.';
+  bg.style.backgroundColor = state.color;
   console.log('Confidence: ' + event.results[0][0].confidence);
 }
 
@@ -64,3 +80,46 @@ recognition.onnomatch = function(event) {
 recognition.onerror = function(event) {
   diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
 }
+
+
+
+new Vue({
+  el: '#mover-container',
+  template: '<div id="mover" :style="styles"><span>Moving {{state.direction}}</span></div>',
+  data() {
+    return {
+      state
+    }
+  },
+  computed: {
+    styles: function() {
+      return {'bottom': (this.state.bottom * 100) + '%', 'left': (this.state.left * 100) + '%', 'background-color': this.state.color};
+    }
+  },
+  watch: {
+    'state.direction': function(newVal, oldVal) {
+      if (oldVal !== newVal) {
+        switch (newVal) {
+          case 'up':
+            state.bottom = state.bottom + 0.1;
+            if (state.bottom > 1) state.bottom = 1;
+            break;
+          case 'down':
+            state.bottom = state.bottom - 0.1;
+            if (state.bottom < 0) state.bottom = 0;
+            break;
+          case 'left':
+            state.left = state.left + 0.1;
+            if (state.left > 1) state.left = 1;
+            break;
+          case 'left':
+            state.left = state.left - 0.1;
+            if (state.left < 0) state.left = 0;
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  }
+});
